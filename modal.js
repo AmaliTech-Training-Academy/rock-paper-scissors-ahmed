0fc212d0fec4b9324864e4d34d4443b5da3faf05
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-03-07 16:37:42 +0000

    added bonus rules modal

--- modal.js
+++ modal.js
@@ -2,7 +2,12 @@ const openRules = window.document.querySelector('[data-modal-target]'); //opens 
 const closeRules = window.document.querySelector('[data-close-button]'); //closes rules modal
 const overlay = window.document.querySelector('#overlay');
 const modal = window.document.querySelector('#modal'); // gets the modal
+const advance = window.document.querySelector('#advanced'); // gets the modal
+const openAdvanced = window.document.querySelector('[data-target]')
+const closeAdvanced = window.document.querySelector('[data-close]')
 
+
+//rules modal
 openRules.addEventListener('click', () => openModal());
 closeRules.addEventListener('click', () => closeModal());
 
@@ -18,3 +23,18 @@ const closeModal = () => {
   overlay.classList.remove('active')
 }
 
+//advanced modal
+openAdvanced.addEventListener('click', () => openAdvance());
+closeAdvanced.addEventListener('click', () => closeAdvance());
+
+const openAdvance = () => {
+  if(advance === null) return 
+  advance.classList.add('active')
+  overlay.classList.add('active')
+}
+
+const closeAdvance = () => {
+  if(advance === null) return 
+  advance.classList.remove('active')
+  overlay.classList.remove('active')
+}
